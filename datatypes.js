402753199dd132d61f9bde7fe78ec7a9e5fe871b
Gokul Malik <131577760+Gokulmlk@users.noreply.github.com>
2024-08-12 14:10:39 +0000

    datatype

---
+++ datatypes.js
@@ -0,0 +1,13 @@
+/*In javascript there are two types of data types are there such as */
+//premitive
+let number = 5// number -> ranges from 0 to 2 to power 53
+let bigInt = 1000000000000000000000000000000000000000000000000000000000// bigInt -> for big number
+let isLogged = false// boolean -> true/false
+let temp = null// null -> a stand aalon value
+let name = "Power"// string -> ""
+let state;// undefined
+// symbol -> for unique
+
+
+//non-premitive
+//object
